18fea2cc0ce643b471d5e24c45c7d7f2dd2b8808
michaelekwandja <145231541+michaelekwandja@users.noreply.github.com>
2024-09-08 11:35:04 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace InsuranceApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_register = new System.Windows.Forms.Button();
+            this.tb_phoneNumber = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.rbtn_female = new System.Windows.Forms.RadioButton();
+            this.rbtn_male = new System.Windows.Forms.RadioButton();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tb_id = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tb_lastName = new System.Windows.Forms.TextBox();
+            this.dt_dateOfBirth = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tb_firstName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cb_doYouSmoke = new System.Windows.Forms.ComboBox();
+            this.cb_doYouExercise = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cb_doYouPlaySport = new System.Windows.Forms.ComboBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_register
+            // 
+            this.btn_register.AutoSize = true;
+            this.btn_register.BackColor = System.Drawing.Color.Transparent;
+            this.btn_register.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_register.ForeColor = System.Drawing.Color.ForestGreen;
+            this.btn_register.Location = new System.Drawing.Point(352, 327);
+            this.btn_register.Name = "btn_register";
+            this.btn_register.Size = new System.Drawing.Size(109, 41);
+            this.btn_register.TabIndex = 33;
+            this.btn_register.Text = "Get Price";
+            this.btn_register.UseVisualStyleBackColor = false;
+            // 
+            // tb_phoneNumber
+            // 
+            this.tb_phoneNumber.Font = new System.Drawing.Font("Cooper Black", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_phoneNumber.Location = new System.Drawing.Point(518, 152);
+            this.tb_phoneNumber.Multiline = true;
+            this.tb_phoneNumber.Name = "tb_phoneNumber";
+            this.tb_phoneNumber.Size = new System.Drawing.Size(232, 30);
+            this.tb_phoneNumber.TabIndex = 32;            // 
+            // label9
+            // 
+            this.label9.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(412, 149);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(100, 30);
+            this.label9.TabIndex = 31;
+            this.label9.Text = "Phone n.o:";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // rbtn_female
+            // 
+            this.rbtn_female.AutoSize = true;
+            this.rbtn_female.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtn_female.Location = new System.Drawing.Point(308, 156);
+            this.rbtn_female.Name = "rbtn_female";
+            this.rbtn_female.Size = new System.Drawing.Size(84, 23);
+            this.rbtn_female.TabIndex = 30;
+            this.rbtn_female.TabStop = true;
+            this.rbtn_female.Text = "Female";
+            this.rbtn_female.UseVisualStyleBackColor = true;
+            // 
+            // rbtn_male
+            // 
+            this.rbtn_male.AutoSize = true;
+            this.rbtn_male.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtn_male.Location = new System.Drawing.Point(191, 156);
+            this.rbtn_male.Name = "rbtn_male";
+            this.rbtn_male.Size = new System.Drawing.Size(64, 23);
+            this.rbtn_male.TabIndex = 29;
+            this.rbtn_male.TabStop = true;
+            this.rbtn_male.Text = "Male";
+            this.rbtn_male.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(33, 152);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(121, 30);
+            this.label8.TabIndex = 28;
+            this.label8.Text = "Gender:";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // tb_id
+            // 
+            this.tb_id.Font = new System.Drawing.Font("Cooper Black", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_id.Location = new System.Drawing.Point(518, 99);
+            this.tb_id.Multiline = true;
+            this.tb_id.Name = "tb_id";
+            this.tb_id.Size = new System.Drawing.Size(232, 30);
+            this.tb_id.TabIndex = 27;
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(412, 99);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(100, 30);
+            this.label7.TabIndex = 26;
+            this.label7.Text = "ID:";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // tb_lastName
+            // 
+            this.tb_lastName.Font = new System.Drawing.Font("Cooper Black", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_lastName.Location = new System.Drawing.Point(518, 48);
+            this.tb_lastName.Multiline = true;
+            this.tb_lastName.Name = "tb_lastName";
+            this.tb_lastName.Size = new System.Drawing.Size(232, 30);
+            this.tb_lastName.TabIndex = 25;
+            // 
+            // dt_dateOfBirth
+            // 
+            this.dt_dateOfBirth.Font = new System.Drawing.Font("Cooper Black", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dt_dateOfBirth.Location = new System.Drawing.Point(160, 107);
+            this.dt_dateOfBirth.Name = "dt_dateOfBirth";
+            this.dt_dateOfBirth.Size = new System.Drawing.Size(232, 22);
+            this.dt_dateOfBirth.TabIndex = 24;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(29, 103);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(125, 30);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "Date Of Birth:";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(412, 48);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 30);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "Last Name:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tb_firstName
+            // 
+            this.tb_firstName.Font = new System.Drawing.Font("Cooper Black", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_firstName.Location = new System.Drawing.Point(160, 48);
+            this.tb_firstName.Multiline = true;
+            this.tb_firstName.Name = "tb_firstName";
+            this.tb_firstName.Size = new System.Drawing.Size(232, 30);
+            this.tb_firstName.TabIndex = 21;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(50, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 30);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "First Name:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label3.Font = new System.Drawing.Font("Snap ITC", 16F);
+            this.label3.ForeColor = System.Drawing.Color.Green;
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(800, 45);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Estimate pice for Insurance Policy";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(249, 203);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 30);
+            this.label1.TabIndex = 34;
+            this.label1.Text = "Do you smoke? ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cb_doYouSmoke
+            // 
+            this.cb_doYouSmoke.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cb_doYouSmoke.FormattingEnabled = true;
+            this.cb_doYouSmoke.Items.AddRange(new object[] {
+            "",
+            "Yes",
+            "No"});
+            this.cb_doYouSmoke.Location = new System.Drawing.Point(401, 206);
+            this.cb_doYouSmoke.Name = "cb_doYouSmoke";
+            this.cb_doYouSmoke.Size = new System.Drawing.Size(121, 27);
+            this.cb_doYouSmoke.TabIndex = 35;
+            // 
+            // cb_doYouExercise
+            // 
+            this.cb_doYouExercise.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cb_doYouExercise.FormattingEnabled = true;
+            this.cb_doYouExercise.Items.AddRange(new object[] {
+            "",
+            "Yes",
+            "No"});
+            this.cb_doYouExercise.Location = new System.Drawing.Point(401, 239);
+            this.cb_doYouExercise.Name = "cb_doYouExercise";
+            this.cb_doYouExercise.Size = new System.Drawing.Size(121, 27);
+            this.cb_doYouExercise.TabIndex = 37;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(233, 236);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 30);
+            this.label2.TabIndex = 36;
+            this.label2.Text = "Do you exercise? ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cb_doYouPlaySport
+            // 
+            this.cb_doYouPlaySport.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cb_doYouPlaySport.FormattingEnabled = true;
+            this.cb_doYouPlaySport.Items.AddRange(new object[] {
+            "",
+            "Yes",
+            "No"});
+            this.cb_doYouPlaySport.Location = new System.Drawing.Point(401, 272);
+            this.cb_doYouPlaySport.Name = "cb_doYouPlaySport";
+            this.cb_doYouPlaySport.Size = new System.Drawing.Size(121, 27);
+            this.cb_doYouPlaySport.TabIndex = 39;
+            // 
+            // label10
+            // 
+            this.label10.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(213, 269);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(170, 30);
+            this.label10.TabIndex = 38;
+            this.label10.Text = "Do you play sport? ";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 472);
+            this.Controls.Add(this.cb_doYouPlaySport);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.cb_doYouExercise);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cb_doYouSmoke);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_register);
+            this.Controls.Add(this.tb_phoneNumber);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.rbtn_female);
+            this.Controls.Add(this.rbtn_male);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.tb_id);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.tb_lastName);
+            this.Controls.Add(this.dt_dateOfBirth);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tb_firstName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Name = "Form1";
+            this.Text = "InsuranceApp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_register;
+        private System.Windows.Forms.TextBox tb_phoneNumber;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.RadioButton rbtn_female;
+        private System.Windows.Forms.RadioButton rbtn_male;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox tb_id;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tb_lastName;
+        private System.Windows.Forms.DateTimePicker dt_dateOfBirth;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tb_firstName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cb_doYouSmoke;
+        private System.Windows.Forms.ComboBox cb_doYouExercise;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cb_doYouPlaySport;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace InsuranceApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private class RegisterInsurancePolicy
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public DateTime DateOfBirth { get; set; }
+            public string ID { get; set; }
+            public RadioButton Gender { get; set; }
+            public string PhoneNumber { get; set; }
+
+            private int Price;
+
+            public RegisterInsurancePolicy(string firstName, string lastName, DateTime dateOfBirth, string id, RadioButton gender, string phoneNumber, int price)
+            {
+                FirstName = firstName;
+                LastName = lastName;
+                DateOfBirth = dateOfBirth;
+                ID = id;
+                Gender = gender;
+                PhoneNumber = phoneNumber;
+                Price = price;
+            }
+        }
+
+        private void btn_register_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string firstName = tb_firstName.Text;
+                string lastName = tb_lastName.Text;
+                DateTime dateOfBirth = DateTime.Parse(dt_dateOfBirth.Text);
+                string id = tb_id.Text;
+                RadioButton gender = rbtn_male.Checked ? rbtn_male : rbtn_female;
+                string phoneNumber = tb_phoneNumber.Text;
+                int age = DateTime.Now.Year - dateOfBirth.Year;
+                int price = 4000;
+
+                if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName))
+                {
+                    MessageBox.Show("First name or last name is empty.");
+                }
+                else if (id.ToString().Length != 13)
+                {
+                    MessageBox.Show("ID should only contain 13 characters");
+                }
+                else if (phoneNumber.ToString().Length != 10)
+                {
+                    MessageBox.Show("Phone Number should only contain 10 digits only");
+                }
+                else if (age < 18)
+                {
+                    MessageBox.Show("You must be at least 18 years old.");
+                }
+                else if (!rbtn_male.Checked && !rbtn_female.Checked)
+                {
+                    MessageBox.Show("Please select a gender.");
+                }
+                else
+                {
+                    if (cb_doYouSmoke.SelectedItem != null && cb_doYouSmoke.SelectedItem.ToString() == "Yes")
+                    {
+                        price += 1000;
+                    }
+                    if(cb_doYouExercise.SelectedItem != null && cb_doYouExercise.SelectedItem.ToString() == "Yes")
+                    {
+                        price -= 500;
+                    } 
+                    if (cb_doYouPlaySport.SelectedItem != null && cb_doYouPlaySport.SelectedItem.ToString() == "Yes")
+                    {
+                        price += 500;
+                    }
+
+                    MessageBox.Show($"{firstName} {lastName}, Your insurance price\n\a " +
+                        $"Sums up to \n\a" +
+                        $"R {price}");
+
+                    RegisterInsurancePolicy policy = new RegisterInsurancePolicy(firstName, lastName, dateOfBirth, id, gender, phoneNumber, price);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Please complete the form...");
+            }
+            
+
+
+
+
+
+
+
+            
+
+            
+        }
+    }
+}
+
